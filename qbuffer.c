f2e014f17853242661ca7091df90d248ee7e8f22
Leo <lurbina@mit.edu>
2011-04-12 15:56:40 -0400

    Changed qbuffer to be able to handle any kind of pointers

--- qbuffer.c
+++ qbuffer.c
@@ -5,34 +5,37 @@
 #include "qbuffer.h"
 
 void
-q_init(qbuffer_t* buff){
+q_init(qbuffer_t* buff, int max_size){
 
   assert(pthread_mutex_init( &(buff->q_mutex_) , NULL ) == 0 );
   assert(pthread_cond_init( &(buff->q_condv_pop_) , NULL ) == 0 );
   assert(pthread_cond_init( &(buff->q_condv_push_) , NULL ) == 0 );
 
+  buff->max_size = max_size;
   buff->head = 0;
   buff->tail = 0;
   buff->size = 0;
 
+  buff->q_ = malloc(max_size*sizeof(void*));
+
   int i;
-  for(i = 0; i < MAX_Q_SIZE; i++){
+  for(i = 0; i < max_size; i++){
     buff->q_[i] = NULL;
   }
 }
 
 void
-q_push(qbuffer_t* buff, Data_Pckt* packet){
+q_push(qbuffer_t* buff, void* entry){
   assert(pthread_mutex_lock(&buff->q_mutex_) == 0);
   
-  while(buff->size == MAX_Q_SIZE){
+  while(buff->size == buff->max_size){
     pthread_cond_signal( &(buff->q_condv_pop_) );
     pthread_cond_wait( &(buff->q_condv_push_), &(buff->q_mutex_) );
   }
   
   buff->head++;
   buff->size++;
-  buff->q_[buff->head%MAX_Q_SIZE] = packet;
+  buff->q_[buff->head%buff->max_size] = entry;
   
   pthread_cond_signal( &(buff->q_condv_pop_) );
   pthread_cond_signal( &(buff->q_condv_push_) );
@@ -40,7 +43,7 @@ q_push(qbuffer_t* buff, Data_Pckt* packet){
   assert(pthread_mutex_unlock(&buff->q_mutex_) == 0);
 }
 
-Data_Pckt*
+void*
 q_pop(qbuffer_t* buff){
   assert(pthread_mutex_lock(&buff->q_mutex_) == 0);
 
@@ -49,7 +52,7 @@ q_pop(qbuffer_t* buff){
     pthread_cond_wait( &(buff->q_condv_pop_), &(buff->q_mutex_) );
   }
   
-  Data_Pckt* packet = buff->q_[buff->tail%MAX_Q_SIZE];
+  void* entry = buff->q_[buff->tail%buff->max_size];
 
   buff->tail++;
   buff->size--;
@@ -58,20 +61,20 @@ q_pop(qbuffer_t* buff){
   pthread_cond_signal( &(buff->q_condv_pop_) );
 
   assert(pthread_mutex_unlock(&buff->q_mutex_) == 0);
-  return packet;
+  return entry;
 }
 
-
 void
-q_free(qbuffer_t* buff, int begin, int n){
+q_free(qbuffer_t* buff, void (*free_handler)(const void*), int begin, int n){
   assert(pthread_mutex_lock(&buff->q_mutex_) == 0);
   
   int i;
   for(i = 0; i < n; i++){
-    free(buff->q_[(begin+i)%MAX_Q_SIZE]->packet_coeff);
-    free(buff->q_[(begin+i)%MAX_Q_SIZE]->payload);
-    free(buff->q_[(begin+i)%MAX_Q_SIZE]);
+    free_handler(buff->q_[(begin+i)%buff->max_size]);
   }
 
   assert(pthread_mutex_unlock(&buff->q_mutex_) == 0);
 }
+
+
+
--- qbuffer.h
+++ qbuffer.h
@@ -10,23 +10,22 @@
  * implementation.
  */
 
-#define MAX_Q_SIZE 2*BLOCK_SIZE*sizeof(Data_Pckt*)
-
 typedef struct{
   pthread_mutex_t q_mutex_;
   pthread_cond_t q_condv_pop_;
   pthread_cond_t q_condv_push_;
 
+  int max_size; // Maximum size of the buffer
   int head; // Index of the first element in the queue
   int tail; // Index of the last element in the queue
-  Data_Pckt* q_[MAX_Q_SIZE];
+  void** q_;
 
   int size; // The number of elements currently in the queue
 } qbuffer_t;
 
 void q_init(qbuffer_t* buff);
-void q_push(qbuffer_t* buff, Data_Pckt* packet);
-Data_Pckt* q_pop(qbuffer_t* buff);
+void q_push(qbuffer_t* buff, void* entry);
+void* q_pop(qbuffer_t* buff);
 void q_free(qbuffer_t* buff, int begin, int n);
 
 
