6dd8b3b908916125d7cf3e3aebc6e210d2014792
Leo <lurbina@mit.edu>
2011-06-16 12:02:15 -0400

    Modified Fabric file to: 1) Call server with log name, 2) Specify file, user and config file

--- fabfile.py
+++ fabfile.py
@@ -10,8 +10,9 @@ info = { 'ctcp'       : '18.62.16.187',
          'localhost'  : '127.0.0.1'
          }
 
+
 def host(user):
-    print "running setup with user " + user
+    print "Running setup with user " + user
     if not info.has_key(user):
         raise Exception('The user ' + str(user) + ' does not exist')
     else:
@@ -20,26 +21,37 @@ def host(user):
 
 class ThreadRunServer(threading.Thread):
     '''Run a server instance locally'''
-    def __init__(self, remake=False):
+    def __init__(self, config, remake):
         self.remake_ = remake
+        self.config_ = config
         threading.Thread.__init__(self)
 
 
     def run(self, remake=False):
         if self.remake_:
             local('make remake')
-        local('./srvctcp config/vegas')
+        now = datetime.now()
+        local('./srvctcp'   +
+              ' -c config/' + self.config_ +
+              ' -l logs/%(year)d-%(month)02d-%(day)02d %(hour)02d:%(minute)02d.%(second)02d.log' %
+              {'year'   : now.year,
+               'month'  : now.month,
+               'day'    : now.day,
+               'hour'   : now.hour,
+               'minute' : now.minute,
+               'second' : now.second })
 
-def start_client(localIP, remake=False):
+def start_client(localIP, file, remake):
+    '''Run a client instance remotely with the user and host specified by env.user and env.hosts'''
     with cd(code_dir):
         if remake:
             run('make remake')
-        run('./clictcp -f shrimp.avi -h ' + str(localIP))
+        run('./clictcp -f ' + file + ' -h ' + str(localIP))
 
-def test(localIP, user='ctcp', remake=False):
-    t = ThreadRunServer(remake)
+def test(localIP, user='ctcp', file='shrimp.avi', config='vegas', remake=False):
+    t = ThreadRunServer(config, remake)
     t.start()
-    start_client(localIP, remake)
+    start_client(localIP, file, remake)
     t.join()
 
 
