a99f6561a65e1dde3ad8968dd2d771dfcf180cd9
Doug Leith <doug.leith@nuim.ie>
2012-10-28 23:04:21 +0000

    Added goodput to ctcp-probe output

--- src/srvctcp.c
+++ src/srvctcp.c
@@ -1348,9 +1348,9 @@ handle_ack(srvctcp_sock* sk, Ack_Pckt *ack, int pin){
 
     log_srv_status(sk);
     if (sk->curr_block ==1)
-       subpath->goodput = sk->blocks[sk->curr_block%NUM_BLOCKS].len *PAYLOAD_SIZE;
+       subpath->goodput = sk->blocks[sk->curr_block%NUM_BLOCKS].len;
     else
-       subpath->goodput += sk->blocks[sk->curr_block%NUM_BLOCKS].len *PAYLOAD_SIZE;
+       subpath->goodput += sk->blocks[sk->curr_block%NUM_BLOCKS].len;
 
     freeBlock(&(sk->blocks[sk->curr_block%NUM_BLOCKS]));
     
@@ -1378,7 +1378,7 @@ handle_ack(srvctcp_sock* sk, Ack_Pckt *ack, int pin){
      
     if (sk->debug > 2 && sk->curr_block%1==0){
       printf("Now sending block %d, cwnd %d, SLR %f%%, SRTT %f ms, MINRTT %f ms, BASERTT %f ms, RATE %f Mbps (%f/%f), win %d, SRTT_user %f \n",
-             sk->curr_block, subpath->snd_cwnd, 100*subpath->slr, subpath->srtt*1000, subpath->minrtt*1000, subpath->basertt*1000, 8.e-6*(subpath->rate*PAYLOAD_SIZE), 8.e-6*(subpath->snd_una*MSS)/(getTime() - sk->start_time), 8.e-6*subpath->goodput/(getTime() - sk->start_time), subpath->snd_cwnd - (subpath->snd_nxt - subpath->snd_una), subpath->srtt_user*1000);
+             sk->curr_block, subpath->snd_cwnd, 100*subpath->slr, subpath->srtt*1000, subpath->minrtt*1000, subpath->basertt*1000, 8.e-6*(subpath->rate*PAYLOAD_SIZE), 8.e-6*(subpath->snd_una*MSS)/(getTime() - sk->start_time), 8.e-6*subpath->goodput*PAYLOAD_SIZE/(getTime() - sk->start_time), subpath->snd_cwnd - (subpath->snd_nxt - subpath->snd_una), subpath->srtt_user*1000);
     }
   }
 
@@ -1538,12 +1538,12 @@ ctcp_probe(srvctcp_sock* sk, int pin) {
    }
 
    if (sk->db) {
-        fprintf(sk->db,"%f dest %s:%u  %d %#x %#x %u %u %u %u %u %u %u %f %f\n",
+        fprintf(sk->db,"%f dest %s:%u  %d %#x %#x %u %u %u %u %u %u %u %f %f %f\n",
            getTime(), sk->clientip, sk->clientport,
            MSS, subpath->snd_nxt, subpath->snd_una,
            subpath->snd_cwnd, subpath->snd_ssthresh, MAX_CWND,
            (int) (subpath->srtt*1000), (int) (subpath->basertt*1000), (int) (subpath->rtt*1000), (int) (subpath->minrtt*1000),
-           100*subpath->slr, subpath->rate*PAYLOAD_SIZE*8e-6);
+           100*subpath->slr, subpath->rate*PAYLOAD_SIZE*8e-6, subpath->goodput*PAYLOAD_SIZE*8e-6);
         fflush(sk->db);
    }
 }
